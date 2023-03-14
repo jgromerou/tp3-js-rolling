d3fcf7b98111aba946cf7e4c236c3e39e08f2bef
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-03-14 14:23:31 -0300

    ArraysEjercicio1 realizado

--- js/arraysEjercicio1.js
+++ js/arraysEjercicio1.js
@@ -1,1 +1,22 @@
-console.log('Estructura básica del TP N° 3');
+/*1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista  los doce nombres del arreglo. */
+let meses = [
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre',
+];
+
+document.write('<h2>Listado de Meses</h2>');
+document.write('<ul>');
+for (let mes = 0; mes < meses.length; mes++) {
+  document.write(`<li>${meses[mes]}</li>`);
+}
+document.write('</ul>');
